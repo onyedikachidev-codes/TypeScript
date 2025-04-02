1e52e43a00a3a4e20622b4105364b1ed9ecb23e8
Onyedikachi Nwanguma <nwangumabimma@gmail.com>
2025-04-02 01:06:17 +0100

    thirdParty done

--- thirdPartyLib.ts
+++ thirdPartyLib.ts
@@ -50,3 +50,12 @@ function printUser(user: User) {
   console.log(user.email);
   console.log(user.phone);
 }
+
+//$ npm install --save-dev @types/lodash for lodash types
+
+//Change your module in ts-config to es6 or es2015
+//Then in your html change type="module"
+
+//import type when importing things like interfaces
+//Always end imports with .js not .ts
+//Example import type {Person} from "./dist/types.js"
